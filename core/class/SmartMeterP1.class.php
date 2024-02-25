004d92c9302076f57365003b7fa878a6178cd6a3
Mips2648 <mips2648@gmail.com>
2024-02-25 16:54:04 +0100

    add power by phase & review log level

--- core/class/SmartMeterP1.class.php
+++ core/class/SmartMeterP1.class.php
@@ -183,9 +183,9 @@ class SmartMeterP1 extends eqLogic {
 			$f = fsockopen($host, $port, $cfgTimeOut);
 
 			if (!$f) {
-				log::add(__CLASS__, 'warning', "Cannot connect to {$this->getName()} ({$host}:{$port})");
+				log::add(__CLASS__, 'error', "Cannot connect to {$this->getName()} ({$host}:{$port})");
 			} else {
-				log::add(__CLASS__, 'info', "Connected to {$this->getName()} ({$host}:{$port})");
+				log::add(__CLASS__, 'debug', "Connected to {$this->getName()} ({$host}:{$port})");
 
 				$codes = [
 					"1.8.1",	// import high
@@ -199,67 +199,82 @@ class SmartMeterP1 extends eqLogic {
 					"72.7.0",	// voltage 3
 					"31.7.0",	// intensity 1
 					"51.7.0",	// intensity 1
-					"71.7.0"	// intensity 1
+					"71.7.0",	// intensity 1
+					"21.7.0",	// import power 1
+					"41.7.0",	// import power 2
+					"61.7.0",	// import power 3
+					"22.7.0",	// export power 1
+					"42.7.0",	// export power 2
+					"62.7.0"	// export power 3
+				];
+				$unused_codes = [
+					"1.4.0", // power last quarter; not used in plugin
+					"17.0.0" // power limit for client with pre-paid contract; not used in plugin
 				];
 				$fullregex = '/\d\-\d:(\d+\.\d+\.\d+)\((\d+\.\d{1,3})\*([VAkWh]+){1,3}\)/';
 				$coderegex = '/\d\-\d:(\d+\.\d+\.\d+)\((.*)\)/';
 				$results = [];
-				while (($data =  fgets($f, 4096)) !== false) {
+				while (($line =  fgets($f, 4096)) !== false) {
+					$line = trim($line);
+					if (empty($line)) continue;
+					log::add(__CLASS__, 'debug', "Parse: {$line}");
 					$matches = [];
-					if (preg_match($fullregex, $data, $matches) === 1) {
-						$current_code = $matches[1];
-						if (in_array($current_code, $codes)) {
+					if (preg_match($fullregex, $line, $matches) === 1) {
+						$code = $matches[1];
+						if (in_array($code, $codes)) {
 							$value = $matches[2];
 							$unit = $matches[3];
-							// log::add(__CLASS__, 'debug', "{$current_code}: {$value} {$unit}");
 							if ($unit === 'kW') {
 								$value *= 1000;
 							}
-							$this->checkAndUpdateCmd($current_code, $value);
-							$results[$current_code] = $value;
-						} else {
-							// log::add(__CLASS__, 'debug', "Unknown code {$current_code}");
+							$this->checkAndUpdateCmd($code, $value);
+							$results[$code] = $value;
+						} elseif (!in_array($code, $unused_codes)) {
+							log::add(__CLASS__, 'warning', "Unknown code {$code}: {$line}");
 						}
-					} elseif (preg_match($coderegex, $data, $matches) === 1) {
-						$current_code = $matches[1];
-						$current_data = $matches[2];
+					} elseif (preg_match($coderegex, $line, $matches) === 1) {
+						$code = $matches[1];
+						$data = $matches[2];
 
-						switch ($current_code) {
-							case '1.0.0':
-								// datetime; ex:'240118094756W' => 24/01/18 09:47:56
+						switch ($code) {
+							case '1.0.0': // datetime; ex:'240118094756W' => 24/01/18 09:47:56
+							case '1.6.0': // max power / quarter this month
+							case '31.4.0': // current limit
+							case '96.3.10': // breaker state?
+							case '98.1.0':
 								// not usefull
 								break;
 							case '96.1.1': // serial number
 							case '96.1.4': // id
-								$this->checkAndUpdateCmd($current_code, $current_data);
+								$this->checkAndUpdateCmd($code, $data);
 								break;
 							case '96.14.0': // day/night
-								$this->checkAndUpdateCmd($current_code, $current_data == '0001');
+								$this->checkAndUpdateCmd($code, $data == '0001');
 								break;
 							case '96.13.0': // message and last code from the run
-								if ($current_data != '') {
-									log::add(__CLASS__, 'info', "Message received: {$current_code}={$current_data}");
+								if ($data != '') {
+									log::add(__CLASS__, 'info', "Message received: {$code}={$data}");
 								}
 								$this->checkAndUpdateCmd('totalImport', $results['1.8.1'] + $results['1.8.2']);
 								$this->checkAndUpdateCmd('totalExport', $results['2.8.1'] + $results['2.8.2']);
 								$this->checkAndUpdateCmd('Import-Export', $results['1.7.0'] - $results['2.7.0']);
-								// log::add(__CLASS__, 'debug', "============");
 								break 2; // break from switch & while because last code from the run
 							default:
-								log::add(__CLASS__, 'debug', "additional unused data: {$current_code}={$current_data}");
+								log::add(__CLASS__, 'warning', "Unknown data: {$code}={$data}");
 								break;
 						}
 					} else {
-						// log::add(__CLASS__, 'debug', "cannot extract actual code & value from raw data: {$data}");
+						// log::add(__CLASS__, 'debug', "cannot extract actual code & value from raw data: {$line}");
 					}
 				}
 			}
 		} catch (\Throwable $th) {
 			log::add(__CLASS__, 'error', "Error with {$this->getName()} ({$host}:{$port}): {$th->getMessage()}");
 		} finally {
-			log::add(__CLASS__, 'info', "Closing connection to {$this->getName()} ({$host}:{$port})");
+			log::add(__CLASS__, 'debug', "Closing connection to {$this->getName()} ({$host}:{$port})");
 			fclose($f);
 		}
+		log::add(__CLASS__, 'info', "Successfuly refreshed values of {$this->getName()} ({$host}:{$port})");
 	}
 
 	private static function getTopicPrefix() {
