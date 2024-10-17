22c719f7d3817b0b746e7ba15bb9b329f744f869
Mips2648 <mips2648@gmail.com>
2024-10-17 16:25:57 +0200

    restore vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '052381f844190ddcc97ce6f9565c3e90af47e3cb',
+    'name' => 'mips/jeedom-smartmeterp1',
+  ),
+  'versions' => 
+  array (
+    'mips/jeedom-smartmeterp1' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '052381f844190ddcc97ce6f9565c3e90af47e3cb',
+    ),
+    'mips/jeedom-tools' => 
+    array (
+      'pretty_version' => 'v1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'bb8a7b65641ab20e93ba4d2a239ab2a6a7bc34af',
+    ),
+  ),
+);
