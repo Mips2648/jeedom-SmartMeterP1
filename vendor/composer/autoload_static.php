a6883e38947d00a1d4e7e61e6fef29ddd859cf63
Mips2648 <mips2648@gmail.com>
2025-02-21 09:14:45 +0100

    arrange composer

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,26 +4,29 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit4b24badd95ef4c09cb48a8ed715eace6 {
-    public static $prefixesPsr0 = array(
-        'M' =>
-        array(
-            'MipsEqLogicTrait' =>
-            array(
+class ComposerStaticInit4b24badd95ef4c09cb48a8ed715eace6
+{
+    public static $prefixesPsr0 = array (
+        'M' => 
+        array (
+            'MipsEqLogicTrait' => 
+            array (
                 0 => __DIR__ . '/..' . '/mips/jeedom-tools/src',
             ),
         ),
     );
 
-    public static $classMap = array(
+    public static $classMap = array (
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
         'MipsEqLogicTrait' => __DIR__ . '/..' . '/mips/jeedom-tools/src/MipsEqLogicTrait.php',
     );
 
-    public static function getInitializer(ClassLoader $loader) {
+    public static function getInitializer(ClassLoader $loader)
+    {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixesPsr0 = ComposerStaticInit4b24badd95ef4c09cb48a8ed715eace6::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit4b24badd95ef4c09cb48a8ed715eace6::$classMap;
+
         }, null, ClassLoader::class);
     }
 }
